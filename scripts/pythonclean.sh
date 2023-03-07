94590d2e881c7066e6abfaf6fb9e7e49b3034d74
nicolasvanz <nicolas.vanz+github@outlook.com>
2023-03-07 16:53:47 -0300

    [scripts] remove python cache when cleaning directory

--- scripts/pythonclean.sh
+++ scripts/pythonclean.sh
@@ -2,5 +2,8 @@ DIR_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd  )"
 
 source $DIR_SCRIPT/const.sh
 
+cecho $RED "[+] Cleaning up python cache"
+find . | grep -E "(/__pycache__)" | xargs rm -rf
+
 cecho $RED "[+] Cleaning up python virtual environment"
 rm -rf $DIR_VENV
