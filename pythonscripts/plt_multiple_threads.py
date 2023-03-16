50da9d26e73784eb3dd7b835c34ac623ba4fdc15
Nicolas Vanz <nicavanz@hotmail.com>
2023-03-16 19:06:07 -0300

    [results] modify plot scripts for new dataset

--- pythonscripts/plt_multiple_threads.py
+++ pythonscripts/plt_multiple_threads.py
@@ -9,50 +9,85 @@ from matplotlib import pyplot as plt
 from argparser import init_parser
 
 
-def customize_and_save_plot(originaldf, col, mapx, mapy, outfilesuffix, ylim=(None, None), col_wrap=None):
+def customize_and_save_plot(originaldf, col, mapx, mapy, outfilesuffix, ylim=(None, None), col_wrap=None, xlogarithmic=False, xonlyeven=False):
     df = originaldf.copy()
 
     outputfilepath_name, outputfilepath_ext = os.path.splitext(args.outputfilepath)
 
-    # add logarithmic scale for mapx for uniform spacing in x axis
-    df[mapx] = df[mapx].apply(lambda x : math.log2(x) + 1 if x != 0 else x)
+    if xlogarithmic:
+        # add logarithmic scale for mapx for uniform spacing in x axis
+        df[mapx] = df[mapx].apply(lambda x : math.log2(x) + 1 if x != 0 else x)
 
     # build facet grid
-    facet_gridp = sb.FacetGrid(df, col=col, sharey=False, sharex=False, col_wrap=None)
-    facet_gridp.map(plt.bar, mapx, mapy, color="black")
+    facet_grid = sb.FacetGrid(df, col=col, sharey=False, sharex=False, col_wrap=col_wrap, aspect=1)
+    facet_grid.map(sb.barplot, mapx, mapy, color="black", width=1, dodge=False)
+    
+    # for axe in facet_grid.axes.flat:
+    #     axe.relim()
+    #     axe.autoscale_view()
+    #     axe.margins(y=0.1)
+
+    for axe in facet_grid.axes.flat:
+        for patch in axe.patches:
+            current_width = patch.get_width()
+            diff = current_width - 0.8
+            patch.set_width(0.8)
+            patch.set_x(patch.get_x() + diff * .5)
 
     # set custom xticks
-    facet_gridp.set(
-        xticks=sorted(list(set(df[mapx].to_list()))),
-        xticklabels=sorted(list(set(originaldf[mapx].to_list()))),
-        ylim=ylim
+    xticks = sorted(list(set(df[mapx].to_list())))
+    xtickslabels= sorted(list(set(originaldf[mapx].to_list())))
+    if xonlyeven:
+        xticks = list(filter(lambda x: x%2 == 0, xticks))
+        xtickslabels = list(filter(lambda x: x%2 == 0, xtickslabels))
+
+    facet_grid.set(
+        xticks=xticks,
+        xticklabels=xtickslabels,
+        ylim=ylim,
     )
 
-    for axe in facet_gridp.axes.flat:
+    for ax in facet_grid.axes:
+	    for p in ax.patches:
+             ax.annotate("%d" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()),
+                 ha='center', va='center', fontsize=8, color='black', xytext=(0, 5),
+                 textcoords='offset points')
+
+    for axe in facet_grid.axes.flat:
         # add y axis grid linesplt.
         for yticklabel in axe.get_yticklabels():
             axe.axhline(int(yticklabel.get_text()), color="grey", linewidth=0.005, ls="--")
 
-        axe.set_ylabel("Time (ms)")
+        # axe.set_ylabel("Time (ms)")
 
     # automatically adjust subplots padding
-    facet_gridp.fig.tight_layout()
+    facet_grid.fig.tight_layout()
 
     plt.savefig(outputfilepath_name + outfilesuffix + outputfilepath_ext)
 
 def main():
     df = build_dataframe()
 
+    #rename parameters and columns
+    new_columns_names = {
+        "time":"milissegundos",
+        "pages":"páginas",
+        "threads":"threads"
+    }
+    df = df.rename(columns=new_columns_names)
+
     customize_and_save_plot(
-        df, "pages", "threads", "time", "_pages",
+        df, "páginas", "threads", "milissegundos", "_pages",
         ylim=(0, 120),
-        col_wrap=4
+        col_wrap=3,
+        xonlyeven=True
     )
 
     customize_and_save_plot(
-        df, "threads", "pages", "time", "_threads",
+        df, "threads", "páginas", "milissegundos", "_threads",
         ylim = (0, 120),
-        col_wrap=3
+        col_wrap=3,
+        xlogarithmic=True
     )
 
 def build_dataframe():
