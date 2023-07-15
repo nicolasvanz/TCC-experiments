fd840a0f9d1bbde6d63a517625701ce91c9af707
nicolasvanz <nicavanz@hotmail.com>
2023-07-15 15:07:10 -0300

    [results] increase parallel plot font size

--- pythonscripts/plt_parallel.py
+++ pythonscripts/plt_parallel.py
@@ -7,10 +7,14 @@ from matplotlib import pyplot as plt
 
 from argparser import init_parser
 
+PRINT_INFO=False
+
 def main():
     df = build_dataframe()
     df["clusters"]=df["clusters"].apply(lambda x : int(x/2))
 
+    sb.set(font_scale=1.7)
+    sb.set_style("whitegrid")
     fig = sb.barplot(
         x="clusters", y="time", data=df, errorbar="sd", color="black"
     )
@@ -22,11 +26,12 @@ def main():
     for p in fig.patches:
         fig.annotate(
             "%.2f" % p.get_height(), (p.get_x() + p.get_width() / 2.,
-            p.get_height()), ha='center', va='center', fontsize=9,
+            p.get_height()), ha='center', va='center', fontsize=15,
             color='black', xytext=(0, 5), textcoords='offset points',
         )
 
     plt.ylim(110, 120)
+    plt.tight_layout()
     plt.savefig(args.outputfilepath)
 
 def build_dataframe():
@@ -38,16 +43,17 @@ def build_dataframe():
             clusters = list(map(int, re.split('_|-|\.', filename)[1:2]))[0]
             df = pd.read_csv(filepath)
             mean = df["time"].mean()
-            print(f"\n{filename} statistics: 95% confidence interval and std")
-            # 95% confidence interval
-            print(list(map(lambda x: x/args.frequency * 1000, st.t.interval(
-                confidence=0.95,
-                df=len(df["time"])-1,
-                loc=mean,
-                scale=st.sem(df["time"])
-            ))))
-            # std
-            print(df["time"].std()/args.frequency*1000)
+            if (PRINT_INFO):
+                print(f"\n{filename} statistics: 95% confidence interval and std")
+                # 95% confidence interval
+                print(list(map(lambda x: x/args.frequency * 1000, st.t.interval(
+                    confidence=0.95,
+                    df=len(df["time"])-1,
+                    loc=mean,
+                    scale=st.sem(df["time"])
+                ))))
+                # std
+                print(df["time"].std()/args.frequency*1000)
             mean = mean / args.frequency * 1000 #milliseconds
             df_lines.append([clusters, mean])
     df = pd.DataFrame(df_lines, columns=["clusters", "time"])
